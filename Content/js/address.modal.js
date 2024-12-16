0124702db48e40624b6355278103d27254442ca2
mike <mike@I7MEDIA-14>
2024-12-16 08:23:56 -0600

    Mostly working

--- Content/js/address.modal.js
+++ Content/js/address.modal.js
@@ -18,8 +18,10 @@ const
         const el = document.getElementById('existingprimaryaddresses'),
             changeEvent = new Event('change');
 
-        el.value = `${id}`;
-        el.dispatchEvent(changeEvent);
+        if (!!el) {
+            el.value = id;
+            el.dispatchEvent(changeEvent);
+        }
 
         markShippingAddress(id);
     },
@@ -27,18 +29,42 @@ const
         onUserSelectNewAddress(id);
         newAddressDialog.showModal();
     },
+    onCheckBox_Change = ({ target }) => {
+        const unChecked = "Select an Alternative Shipping Address",
+            checked = "Use Same Address for Shipping and Billing";
+        btnUseSecondaryAddress.innerHTML = target.checked ? checked : unChecked;
+    },
     onUserSubmitAddress_Click = async () => {
-        await window.adjustChangeAddressMethod?.(1);
+        const address = window.getPrimaryAddress?.() ?? {},
+            fieldSet = document.querySelector(`[data-addressid="${address.Id}"]`),
+            street = fieldSet.querySelector('[data-street]'),
+            city = fieldSet.querySelector('[data-city]'),
+            zip = fieldSet.querySelector('[data-zip]');
 
-        closeAddressModal();
+        street.innerText = address.Address1;
+        city.innerText = address.City;
+        zip.innerText = address.ZipPostalCode;
+
+        try {
+            await window.adjustChangeAddressMethod?.(1);
+            closeAddressModal();
+        } catch (error) {
+            console.error(error);
+            alert('Unable to Save Address');
+        }
     },
     getPrimaryAddressId = () => {
         const primaryAddressId = document.getElementById('PrimaryAddress_Id');
         return primaryAddressId?.value ?? '0';
     },
+    addEventListeners = () => {
+        btnUseSecondaryAddress.addEventListener('click', () => chxUseSecondaryAddress.click());
+        btnNewAddress?.addEventListener('click', onUserAddNewOrEditAddress_Click);
+        btnSubmitAddress.addEventListener('click', onUserSubmitAddress_Click);
+        chxUseSecondaryAddress.addEventListener('change', onCheckBox_Change);
+        chxUseSecondaryAddress.addEventListener('cancel', onCheckBox_Change);
+    },
     closeAddressModal = () => newAddressDialog.close();
 
+addEventListeners();
 markShippingAddress(getPrimaryAddressId());
-btnUseSecondaryAddress.addEventListener('click', () => chxUseSecondaryAddress.click());
-btnNewAddress.addEventListener('click', onUserAddNewOrEditAddress_Click);
-btnSubmitAddress.addEventListener('click', onUserSubmitAddress_Click);
