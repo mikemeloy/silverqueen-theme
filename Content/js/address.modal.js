53f7db05271cc3700bbdcc58f3c9cd1964cb4361
mike <mike@I7MEDIA-14>
2024-12-17 14:10:13 -0600

    Style changes

--- Content/js/address.modal.js
+++ Content/js/address.modal.js
@@ -1,9 +1,22 @@
+let validation = {
+    streetRequired: false,
+    cityRequired: false,
+    zipRequired: false,
+    phoneRequired: false
+};
+
 const
     btnUseSecondaryAddress = document.querySelector('[data-open-other]'),
     chxUseSecondaryAddress = document.getElementById('UseSecondaryAddress'),
     btnNewAddress = document.querySelector('[data-open-new]'),
     newAddressDialog = document.querySelector('dialog[data-new-address]'),
     btnSubmitAddress = document.querySelector('[data-billing-address-submit]'),
+    btnCloseSecondaryAddress = document.querySelector('[data-shipping-address] i'),
+    isBillingAddressValid = () => {
+        const isValid = window.validatePrimaryAddress?.();
+
+        return isValid;
+    },
     markShippingAddress = (id) => {
         const addressEls = document.querySelectorAll(`[data-addressid]`);
 
@@ -29,21 +42,25 @@ const
         onUserSelectNewAddress(id);
         newAddressDialog.showModal();
     },
-    onCheckBox_Change = ({ target }) => {
-        const unChecked = "Select an Alternative Shipping Address",
-            checked = "Use Same Address for Shipping and Billing";
-        btnUseSecondaryAddress.innerHTML = target.checked ? checked : unChecked;
-    },
     onUserSubmitAddress_Click = async () => {
         const address = window.getPrimaryAddress?.() ?? {},
-            fieldSet = document.querySelector(`[data-addressid="${address.Id}"]`),
+            stateProvince = +address.StateProvinceId;
+
+        if (!isBillingAddressValid() || stateProvince === 0) {
+            alert('Please make sure all required fields have a value');
+            return;
+        }
+
+        const fieldSet = document.querySelector(`[data-addressid="${address.Id}"]`),
             street = fieldSet.querySelector('[data-street]'),
             city = fieldSet.querySelector('[data-city]'),
-            zip = fieldSet.querySelector('[data-zip]');
+            zip = fieldSet.querySelector('[data-zip]'),
+            edit = fieldSet.querySelector('i');
 
         street.innerText = address.Address1;
         city.innerText = address.City;
         zip.innerText = address.ZipPostalCode;
+        edit.innerText = "Edit";
 
         try {
             await window.adjustChangeAddressMethod?.(1);
@@ -61,10 +78,11 @@ const
         btnUseSecondaryAddress.addEventListener('click', () => chxUseSecondaryAddress.click());
         btnNewAddress?.addEventListener('click', onUserAddNewOrEditAddress_Click);
         btnSubmitAddress.addEventListener('click', onUserSubmitAddress_Click);
-        chxUseSecondaryAddress.addEventListener('change', onCheckBox_Change);
-        chxUseSecondaryAddress.addEventListener('cancel', onCheckBox_Change);
+        btnCloseSecondaryAddress.addEventListener('click', () => chxUseSecondaryAddress.click());
     },
     closeAddressModal = () => newAddressDialog.close();
 
 addEventListeners();
 markShippingAddress(getPrimaryAddressId());
+
+
